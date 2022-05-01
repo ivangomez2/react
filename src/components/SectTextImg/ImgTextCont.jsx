e25ef77e25e11e7a8b6b6f8ac9cffea9bab55cc6
ivan <ivansangomez2@gmail.com>
2022-05-01 10:31:23 -0300

    add-Res

--- src/components/SectTextImg/ImgTextCont.jsx
+++ src/components/SectTextImg/ImgTextCont.jsx
@@ -11,7 +11,7 @@ const ImgTextCont = () => {
     parrafo: "Más de 100 diseños unicos para generar un producto exclusivo,personalizamos tu marca en minutos.",
     boton: "Información",
     clase: "ItemPrimImg"
-  },{img: "https://i.ibb.co/KKtv4qg/edit.png" ,
+  },{img: "https://i.ibb.co/bJpFY4Z/Mi-proyecto-3.png" ,
     titulo: "Cobrar nunca fue tan fácil,no pierdas clientes",
     parrafo: "Soporte para mas de +60 medios de pago incluidos MercadoPago - Tarjetas de credito/debito - BTC.",
     boton: "Información",

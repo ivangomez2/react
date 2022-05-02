a104a290f7124ea94409c07255bb2dd4d2fe746a
ivan <ivansangomez2@gmail.com>
2022-05-02 09:36:46 -0300

    ordenandoCards

--- src/items/Item.jsx
+++ src/items/Item.jsx
@@ -16,13 +16,13 @@ const Item = ({prods}) => {
     <img className="card-img-top" src={prods.img} alt="Card image cap"/>
     <div className="card-body">
       <h4 className="card-title">{prods.Nombre}</h4>
-      <p className="card-text">{prods.Descripcion}</p>
+      <p className="card-text" style={{color:"gray"}}>{prods.Descripcion}</p>
    
-      <p className="card-text ">Disponibles {stockAct}</p>
+      <p className="card-text " style={{color:"#0697BB"}}>Disponibles {stockAct}</p>
       <div className="cardCartDiv">
-      <button style={{ backgroundColor:"#0697BB"}} className="badge rounded-pill p-2" onClick={()=>{stock > 0 ? setStock(stock - 1):alert("agrega un prodcuto al menos")}}> - </button>
-      <p className="card-text">En el carro: {Total}</p>
-      <button style={{ backgroundColor:"#0697BB"}} className="badge rounded-pill  p-2" onClick={()=>{stock < myProds ? setStock(stock + 1): alert("Sin Stock Disponible")}} > + </button>
+      <button style={{ backgroundColor:"#0697BB"}} className="Restar" onClick={()=>{stock > 0 ? setStock(stock - 1):alert("agrega un prodcuto al menos")}}> - </button>
+      <p className="card-text buttonItems btn">En el carro: {Total}</p>
+      <button style={{ backgroundColor:"#0697BB"}} className="Sumar" onClick={()=>{stock < myProds ? setStock(stock + 1): alert("Sin Stock Disponible")}} > + </button>
       <a href="#">{prods.link}</a>
 </div>
      
